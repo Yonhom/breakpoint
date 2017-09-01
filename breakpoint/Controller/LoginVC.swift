43c21aaa92e69e58c37d8089589c602a4d381f88
Yonhom <953410459@qq.com>
2017-09-01 22:50:12 +0800

    signup & login page added

--- breakpoint/Controller/LoginVC.swift
+++ breakpoint/Controller/LoginVC.swift
@@ -7,29 +7,75 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
-
+    
+    @IBOutlet weak var emailField: LoginField!
+    
+    @IBOutlet weak var passwordField: LoginField!
+    @IBOutlet weak var progressIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func closePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = emailField.text, let password = passwordField.text { // email & password is not nil
+            // when the login is pressed, show the progress indicator
+            isInProgress(inProgress: true)
+            AuthService.instance.loginUser(withEmail: email, andPassword: password, completion: { (success, error) in
+                if success {
+                    self.isInProgress(inProgress: false)
+                    print("login first attempt successed!")
+                } else {
+                    self.isInProgress(inProgress: true)
+                    print("login first attempt failed, now trying to signing up...")
+                    AuthService.instance.registerUser(withEmail: email, andPassword: password, completion: { (success, error) in
+                        if success {
+                            self.isInProgress(inProgress: true)
+                            print("trying to login for the second time...")
+                            AuthService.instance.loginUser(withEmail: email, andPassword: password, completion: { (success, error) in
+                                if success {
+                                    self.isInProgress(inProgress: false)
+                                    print("login second attempt successed!")
+                                } else {
+                                    self.isInProgress(inProgress: false)
+                                    print("login second attempt failed!")
+                                }
+                            })
+                        } else {
+                            self.isInProgress(inProgress: false)
+                            print("signing up failed with log: \(error.debugDescription)")
+                        }
+                    })
+                }
+            })
+            
+        } else {
+            print("Login failed! Please check your acount again!")
+        }
+        
     }
-    */
-
+    
+    /**
+     * is login or signup in progrees?
+     */
+    func isInProgress(inProgress: Bool) {
+        if inProgress {
+            progressIndicator.isHidden = false
+            loginBtn.isEnabled = false
+        } else {
+            progressIndicator.isHidden = true
+            loginBtn.isEnabled = true
+        }
+    }
+    
 }
--- breakpoint/Other/AppDelegate.swift
+++ breakpoint/Other/AppDelegate.swift
@@ -14,9 +14,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    /**
+     * app instance will call this method after finishing launching
+     */
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         FirebaseApp.configure()
+        
+        // if there isnt a current user, go to the auth page
+        if Auth.auth().currentUser == nil {
+            // get the authVC ref
+            let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            let vc = storyBoard.instantiateViewController(withIdentifier: "AuthVC")
+            
+            // present the authVC
+            window?.makeKeyAndVisible()
+            window?.rootViewController?.present(vc, animated: true, completion: nil)
+        } else {
+            print("Already has a user: \(Auth.auth().currentUser.debugDescription)")
+        }
         
         return true
     }
